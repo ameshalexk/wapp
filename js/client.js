23a47ed7b5db84ea99db694d58b69ee3cfa15f3e
Amesh Alex Kuruvilla <finixbenny@Ameshs-Air.fios-router.home>
2020-08-23 01:05:41 -0400

    added basic send and receive functionality

--- js/client.js
+++ js/client.js
@@ -34,3 +34,7 @@ append(`${name} joined the chat`, 'right')
 socket.on('receive',data => {
     append(`${data.name} : ${data.message}`, 'left')
     });
+
+socket.on('left', name => {
+    append(`${name} left the chat room`, 'left')
+    });
